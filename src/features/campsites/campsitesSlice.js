920e92ce502d6e0a38c3e70fee9414690176ca87
Rachid Amghar <amghar.rachid@gmail.com>
2023-01-11 22:32:54 -0800

    useState

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -6,6 +6,11 @@ export const selectAllCampsites = () => {
   return CAMPSITES;
 }
 
-export const selectRandomCampsite = () => {
-    return CAMPSITES[Math.floor(CAMPSITES.length * Math.random())];
-};
+export const selectCampsiteById = (id) => {
+return CAMPSITES.find((campsite) => campsite.id === id);
+
+}
+
+// export const selectRandomCampsite = () => {
+//     return CAMPSITES[Math.floor(CAMPSITES.length * Math.random())];
+// };
--- src/pages/CampsitesDirectoryPage.js
+++ src/pages/CampsitesDirectoryPage.js
@@ -1,33 +1,27 @@
-import { Container, Row, Col, Button } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
 import CampsiteDetail from '../features/campsites/CampsiteDetail';
 import CampsitesList from '../features/campsites/CampsitesList';
-import { selectRandomCampsite } from '../features/campsites/campsitesSlice';
+import { useState } from 'react';
+//import { selectRandomCampsite } from '../features/campsites/campsitesSlice';
+import { selectCampsiteById } from '../features/campsites/campsitesSlice';
 
 const CampsitesDirectoryPage = () => {
-    let selectedCampsite = selectRandomCampsite();
-    const toggleCampsite = () => {
-        selectedCampsite = selectRandomCampsite();
-        console.log(selectedCampsite);
-    };
+    
+    const [campsiteId, setCampsiteId] = useState(0);
+    const selectedCampsite = selectCampsiteById(campsiteId);
+
     return (
         <Container>
-            <button onClick={() => toggleCampsite()}>
-                Select Random Campsite
-            </button>
+            
             <Row>
                 <Col sm='5' md='7'>
-                    <CampsitesList />
+                    <CampsitesList setCampsiteId={setCampsiteId} />
                 </Col>
                 <Col sm='7' md='5'>
                     <CampsiteDetail campsite={selectedCampsite} />
                 </Col>
             </Row>
         </Container>
-
-
-
     );
-
 };
-
-export default CampsitesDirectoryPage
+export default CampsitesDirectoryPage;
