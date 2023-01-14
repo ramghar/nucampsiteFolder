d91c550095d88b3f705724f7a5105035248db040
Rachid Amghar <amghar.rachid@gmail.com>
2023-01-14 09:06:28 -0800

    Breadcrumbs SubHeader

--- src/pages/CampsitesDirectoryPage.js
+++ src/pages/CampsitesDirectoryPage.js
@@ -1,11 +1,12 @@
 
-
+import SubHeader from '../components/SubHeader';
 import { Container } from 'reactstrap';
 import CampsitesList from '../features/campsites/CampsitesList';
 
 const CampsitesDirectoryPage = () => {
     return (
         <Container>
+            <SubHeader current='Directory'/>
             <CampsitesList />
         </Container>
     );
