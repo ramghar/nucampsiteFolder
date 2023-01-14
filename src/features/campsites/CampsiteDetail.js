9336e9ff972c05bec2e8cd8512f9ace5f43f34c1
Rachid Amghar <amghar.rachid@gmail.com>
2023-01-14 08:53:56 -0800

    Comments

--- src/features/campsites/CampsiteDetail.js
+++ src/features/campsites/CampsiteDetail.js
@@ -1,12 +1,14 @@
 import { Card, CardImg, CardText, CardBody, Col } from 'reactstrap'
 
-const CampsiteDetail = ( { campsite } ) => {
+const CampsiteDetail = ({ campsite }) => {
     const { image, name, description } = campsite;
-    
+
     return (
-        <Col md='12' className='m-4'>
+
+
+        <Col md='5' className='m-1'>
             <Card>
-                <CardImg top src={image} alt={name}/>
+                <CardImg top src={image} alt={name} />
                 <CardBody>
                     <CardText>{description}</CardText>
                 </CardBody>
