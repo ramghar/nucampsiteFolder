0d8b809ac8ec515006a3e18a1f312cd1bb9ca8a2
Rachid Amghar <amghar.rachid@gmail.com>
2023-01-13 20:02:08 -0800

    Homepage Display

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -6,7 +6,13 @@ export const selectAllCampsites = () => {
 }
 
 export const selectCampsiteById = (id) => {
-return CAMPSITES.find((campsite) => campsite.id === id);
+  return CAMPSITES.find((campsite) => campsite.id === id);
 
 }
+
+export const selectFeaturedCampsite = () => {
+  return CAMPSITES.find((campsite) => campsite.featured);
+
+
+};
 
