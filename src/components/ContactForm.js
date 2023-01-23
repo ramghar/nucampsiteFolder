681b7bb87bd90dafe14812890600693536167212
Rachid Amghar <amghar.rachid@gmail.com>
2023-01-22 21:53:14 -0800

    ContactForm Part 3.

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -3,6 +3,11 @@ import { Formik, Field, Form } from 'formik';
 
 
 const ContactForm = () => {
+    const handleSubmit = (values, { resetForm }) => {
+        console.log('form values:', values);
+        console.log('in JSON format:', JSON.stringify(values));
+        resetForm();
+    };
     return (
         <Formik
             initialValues={{
@@ -14,6 +19,7 @@ const ContactForm = () => {
                 contactType: 'By Phone',
                 feedback: ''
             }}
+            onSubmit={handleSubmit}
         >
             <Form>
                 <FormGroup row>
@@ -69,10 +75,15 @@ const ContactForm = () => {
                         Your Feedback
                     </Label>
                     <Col md='10'>
-                        <Field class='form-control' name='feedback' as='textarea' rows='12'/>
+                        <Field class='form-control' name='feedback' as='textarea' rows='12' />
                     </Col>
                 </FormGroup>
                 <FormGroup row>
+                    <Col md={{ size: 10, offset: 2 }}>
+                        <Button type='submit' color='primary'>
+                            Send Feedback
+                        </Button>
+                    </Col>
                 </FormGroup>
             </Form>
 
