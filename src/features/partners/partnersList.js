97254e4304edde2b5dc1dddfe9dedba19e628d05
Rachid Amghar <amghar.rachid@gmail.com>
2023-01-27 23:29:26 -0800

    Redux for All

--- src/features/partners/partnersList.js
+++ src/features/partners/partnersList.js
@@ -1,11 +1,11 @@
 import {Col} from 'reactstrap'
 import Partner from './Partner'
-
+import { useSelector } from 'react-redux'
 import {selectAllPartners} from './partnersSlice'
 
 
 const PartnersList = () => {
-const partners = selectAllPartners();
+const partners = useSelector(selectAllPartners)
 return(
     <Col className='mt-4'>
         {partners.map((partner) => {
