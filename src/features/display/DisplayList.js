d767c21e99b63ea1bba260c084ebaf2cd67257a5
Rachid Amghar <amghar.rachid@gmail.com>
2023-01-27 22:54:09 -0800

    Campsites useSelector

--- src/features/display/DisplayList.js
+++ src/features/display/DisplayList.js
@@ -4,13 +4,16 @@ import AnimatedDisplayCard from './AnimatedDisplayCard'
 import { selectFeaturedCampsite } from '../campsites/campsitesSlice'
 import { selectFeaturedPromotion } from '../promotions/promotionsSlice'
 import { selectFeaturedPartner } from '../partners/partnersSlice'
+import { useSelector } from 'react-redux'
+
 
 const DisplayList = () => {
-    const items = [
-        selectFeaturedPartner(),
-        selectFeaturedCampsite(),
-        selectFeaturedPromotion()
-    ];
+    const items = useSelector((state) => [
+        selectFeaturedCampsite(state),
+        selectFeaturedPromotion(state),
+        selectFeaturedPartner(state)
+    ]);
+    console.log('display items:', items);
     return (
         <Row>
             {items.map((item, idx) => {
