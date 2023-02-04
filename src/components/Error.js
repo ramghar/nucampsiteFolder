1c631e2a16eff94b82ac68f731ec07784ddeaae5
Rachid Amghar <amghar.rachid@gmail.com>
2023-02-04 06:11:11 -0800

    Error and Loading

---
+++ src/components/Error.js
@@ -0,0 +1,14 @@
+
+
+import { Col } from 'reactstrap';
+
+const Error = ({ errMsg }) => {
+    return (
+        <Col>
+            <h4>{errMsg}</h4>
+        </Col>
+    );
+};
+
+export default Error;
+
